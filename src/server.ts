eab5326672867380d8e227be8b194660c86aef0e
Luan Pereira <pereiralv2010@gmail.com>
2024-12-30 03:36:14 -0300

    feat: knex steup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  // const transaction = knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste.',
+  //     amount: 15,
+  //   })
+  //   .returning('*')
+
+  // return transaction
+
+  const transactions = knex('transactions')
+    .where({
+      amount: 15,
+    })
+    .select('*')
+
+  return transactions
 })
 
 app
